ae78b0f7206df2353bb7d7d2ec2c64b9af289574
Frontend JavaScript <frontendjavascript@C3-03.local>
2022-12-08 20:41:08 +0200

    c

---
+++ curs1/curs1_ap1.js
@@ -0,0 +1,6 @@
+alert('Salut Javascript!')
+window.alert('Salut!')
+confirm('Vreau sa invat Javascript!')
+console.log('Mesaj in consola')
+console.info('Info status')
+console.error('Mesaj de eroare')
